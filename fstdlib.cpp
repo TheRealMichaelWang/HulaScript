6eebaf0786c1f4e2f1f462c1e24f806c9c98294e
TheRealMichaelWang <mwang764@gatech.edu>
2024-09-28 09:47:42 -0400

    * Added support for a builtin random generator class

--- fstdlib.cpp
+++ fstdlib.cpp
@@ -3,6 +3,7 @@
 #include "table_iterator.h"
 #include <cstdint>
 #include <memory>
+#include <random>
 
 using namespace HulaScript;
 
@@ -42,7 +43,21 @@ private:
 	}
 };
 
-static instance::value get_int_range(std::vector<instance::value> arguments, instance& instance) {
+class random_generator : public foreign_method_object<random_generator> {
+private:
+	std::mt19937 rng;
+	std::uniform_real_distribution<double> unif_real;
+
+	instance::value next_real(std::vector<instance::value>& arguments, instance& instance) {
+		return instance::value(unif_real(rng));
+	}
+public:
+	random_generator(double lower_bound, double upper_bound) : unif_real(lower_bound, upper_bound) {
+		declare_method("next", &random_generator::next_real);
+	}
+};
+
+static instance::value new_int_range(std::vector<instance::value> arguments, instance& instance) {
 	int64_t start = 0;
 	int64_t step = 1;
 	int64_t stop;
@@ -77,6 +92,21 @@ static instance::value get_int_range(std::vector<instance::value> arguments, ins
 	return instance.add_foreign_object(std::make_unique<int_range>(int_range(start, stop, step)));
 }
 
+static instance::value new_random_generator(std::vector<instance::value> arguments, instance& instance) {
+	if (arguments.size() != 2) {
+		instance.panic("FFI Error: A random generator instance needs a lower bound and an upper bound, two arguments.");
+	}
+
+	double lower_bound = arguments[0].number(instance);
+	double upper_bound = arguments[1].number(instance);
+
+	if (lower_bound >= upper_bound) {
+		instance.panic("FFI Error: A random generator instance cannot have a lower bound greater than or equal to its upper bound.");
+	}
+
+	return instance.add_foreign_object(std::make_unique<random_generator>(random_generator(lower_bound, upper_bound)));
+}
+
 static instance::value sort_table(std::vector<instance::value> arguments, instance& instance) {
 	if (arguments.size() != 2) {
 		instance.panic("FFI Error: Function sort expects 2 arguments: a array-table, and a comparator (return whether left is less than right).");
@@ -104,6 +134,37 @@ static instance::value sort_table(std::vector<instance::value> arguments, instan
 	return instance::value();
 }
 
+static instance::value binary_search_table(std::vector<instance::value> arguments, instance& instance) {
+	if (arguments.size() != 3) {
+		instance.panic("FFI Error: Function sort expects 2 arguments: a array-table, and a comparator (return whether left is less than right), and a key.");
+	}
+
+	HulaScript::ffi_table_helper helper(arguments[0], instance);
+	size_t low = 0;
+	size_t high = helper.size();
+	size_t mid = low;
+
+	while (low <= high) {
+		mid = low + (high - low) / 2;
+
+		bool cmp_res = instance.invoke_value(arguments[1], { helper.at_index(mid), arguments[2] }).boolean(instance);
+		if (cmp_res) {
+			low = mid + 1;
+		}
+		else {
+			bool cmp_res2 = instance.invoke_value(arguments[1], { arguments[2], helper.at_index(mid) }).boolean(instance);
+			if (cmp_res) {
+				high = mid - 1;
+			}
+			else {
+				return instance::value(static_cast<double>(mid));
+			}
+		}
+	}
+
+	return instance::value(-(static_cast<double>(mid) + 1));
+}
+
 instance::value HulaScript::filter_table(instance::value table_value, instance::value keep_cond, instance& instance) {
 	HulaScript::ffi_table_helper helper(table_value, instance);
 	
@@ -120,6 +181,9 @@ instance::value HulaScript::filter_table(instance::value table_value, instance::
 }
 
 instance::instance() {
-	declare_global("irange", make_foreign_function(get_int_range));
+	declare_global("irange", make_foreign_function(new_int_range));
+	declare_global("random", make_foreign_function(new_random_generator));
+
 	declare_global("sort", make_foreign_function(sort_table));
+	declare_global("binarySearch", make_foreign_function(binary_search_table));
 }
