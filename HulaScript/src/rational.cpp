06782f06f6d41d09633280db516f3e9e4b7e561d
TheRealMichaelWang <mwang764@gatech.edu>
2024-11-08 19:13:42 -0500

    * Added support for rational exponentiation and modulo

--- HulaScript/src/rational.cpp
+++ HulaScript/src/rational.cpp
@@ -1,3 +1,5 @@
+#include "HulaScript.h"
+#include "HulaScript.h"
 #include <tuple>
 #include <stdexcept>
 #include "HulaScript.h"
@@ -81,7 +83,6 @@ void instance::handle_rational_multiply(value& a, value& b) {
 		panic("Overflow: Denominator in rational multiplication is too large.");
 	}
 
-	
 	value::vflags flags = ((a.flags & value::vflags::RATIONAL_IS_NEGATIVE) == (b.flags & value::vflags::RATIONAL_IS_NEGATIVE)) ? value::vflags::NONE : value::vflags::RATIONAL_IS_NEGATIVE;
 
 	evaluation_stack.push_back(value(value::vtype::RATIONAL, flags, adenom * bdenom, anum * bnum));
@@ -108,6 +109,72 @@ void instance::handle_rational_divide(value& a, value& b) {
 	value::vflags flags = ((a.flags & value::vflags::RATIONAL_IS_NEGATIVE) == (b.flags & value::vflags::RATIONAL_IS_NEGATIVE)) ? value::vflags::NONE : value::vflags::RATIONAL_IS_NEGATIVE;
 
 	evaluation_stack.push_back(value(value::vtype::RATIONAL, flags, adenom * bnum, anum * bdenom));
+}
+
+void instance::handle_rational_exponentiate(value& a, value& b) {
+	if (b.data.id == 0) {
+		evaluation_stack.push_back(value(value::vtype::RATIONAL, value::vflags::NONE, 1, 1));
+		return;
+	}
+
+	if (b.function_id == 1) {
+		size_t num = 1;
+		size_t denom = 1;
+
+		for (size_t i = 0; i < b.data.id; i++)
+		{
+			if (num > SIZE_MAX / a.data.id || denom > UINT32_MAX / a.function_id) {
+				size_t common_denom = gcd(num, denom);
+				num /= common_denom;
+				denom /= common_denom;
+			}
+
+			if (b.flags & value::vflags::RATIONAL_IS_NEGATIVE) {
+				num *= a.function_id;
+				denom *= a.data.id;
+			}
+			else {
+				num *= a.data.id;
+				denom *= a.function_id;
+			}
+		}
+
+		value::vflags flags = (a.flags & value::vflags::RATIONAL_IS_NEGATIVE && b.data.id % 2 == 1) ? value::vflags::RATIONAL_IS_NEGATIVE : value::vflags::NONE;
+
+		evaluation_stack.push_back(value(value::vtype::RATIONAL, flags, denom, num));
+	}
+	else {
+		evaluation_stack.push_back(value(pow(a.number(*this), b.number(*this))));
+	}
+}
+
+void HulaScript::instance::handle_rational_modulo(value& a, value& b) {
+	size_t anum_bdenom_gcd = gcd(a.data.id, b.data.id);
+	size_t bnum_adenom_gcd = gcd(a.function_id, b.function_id);
+	size_t anum = a.data.id / anum_bdenom_gcd;
+	size_t bnum = b.data.id / anum_bdenom_gcd;
+	size_t adenom = a.function_id / bnum_adenom_gcd;
+	size_t bdenom = b.function_id / bnum_adenom_gcd;
+
+	if (bnum == 0) {
+		panic("Rational: Divide by Zero.");
+	}
+	if (anum > SIZE_MAX / bdenom) {
+		panic("Overflow: Numerator in rational multiplication is too large.");
+	}
+	if (adenom > UINT32_MAX / bnum) {
+		panic("Overflow: Denominator in rational multiplication is too large.");
+	}
+
+	value::vflags flags = ((a.flags & value::vflags::RATIONAL_IS_NEGATIVE) == (b.flags & value::vflags::RATIONAL_IS_NEGATIVE)) ? value::vflags::NONE : value::vflags::RATIONAL_IS_NEGATIVE;
+
+	size_t denom = adenom * bnum;
+	size_t nom = (anum * bdenom) % denom;
+	denom *= b.function_id;
+	nom *= b.data.id;
+	size_t common_denom = gcd(denom, nom);
+	
+	evaluation_stack.push_back(value(value::vtype::RATIONAL, flags, denom / common_denom, nom / common_denom));
 }
 
 instance::value instance::parse_rational(std::string str) const {
