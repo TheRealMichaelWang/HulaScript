2994d40a3ed4efe39ad51e2cdbaf120290642c01
TheRealMichaelWang <mwang764@gatech.edu>
2024-11-22 16:18:29 -0500

    * Added tests for dynamic import

--- examples/import-test.hs
+++ examples/import-test.hs
@@ -13,3 +13,26 @@ function lcm(a, b) do
 		return (b / gcd(a, b)) * a
 	end
 end
+
+function bezout(a, b) do
+	if b == 0 then
+		return [1, 0, a]
+	end
+
+	r = a % b
+	q = (a - r) / b
+
+	g = bezout(b, r)
+	return [g[1], g[0] - q * g[1], g[2]]
+end
+
+function modInv(a, mod) do
+	coefs = bezout(a, mod)
+
+	if coefs[2] != 1 then
+		return nil
+	else
+		inv = coefs[0]
+		return if inv >= 0 then inv else inv + (inv % mod) end
+	end
+end
